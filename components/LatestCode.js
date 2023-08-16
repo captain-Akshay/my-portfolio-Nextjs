8fa0e2f1c3bfe4acc7581955a196c168bd0268b1
captain-Akshay <akshay.sharma0503@gmail.com>
2023-08-16 12:12:04 +0530

    changed few bugs

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -17,7 +17,7 @@ export default function LatestCode({ repositories }) {
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
           <h1
-            className={`text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0  ${
+            className={`text-6xl lg:text-9xl max-w-lg font-bold pb-12 text-gray-500 my-20 md:my-0  ${
               isDark ? "md:text-white" : "md:text-black"
             } dark:text-gray-600 text-center lg:text-left`}
           >
